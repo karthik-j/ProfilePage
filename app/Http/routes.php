3fd712a0cfa897d27156e4a1913b65431796a5f7
karthik-j <karthikj@buffalo.edu>
2015-09-26 15:26:00 -0400

    error test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -53,11 +53,11 @@ The main goal was to provide both availability and linearizability at the same t
         }
     }
 
-    if($filter->url != ""){
-        $output = "<div class='featured_image col-md-6 col-lg-4'>			                <img  src=$filter->img class='' alt=$filter->title title=''>            		</div>		<div class='content col-md-6 col-lg-8'>			<h3 class='single-title'>$filter->title</h3>			<div class='portfolio-content'>	<p><i class=\"fa fa-lg fa-history\"></i> $filter->period</p> <br> <p><i class=\"fa fa-lg fa-github\"></i><a href=\"$filter->url\" target=\"_blank\">$filter->url</a> </p>   <br> <p><b>Skills Used: </b>$filter->skills</p> <br>   <p>$filter->description</p>			</div>					</div><div class='close-view'>				<a href='#'><i class='fa fa-times'></i></a>			</div>";
-    }else{
+//    if($filter->url != ""){
+//        $output = "<div class='featured_image col-md-6 col-lg-4'>			                <img  src=$filter->img class='' alt=$filter->title title=''>            		</div>		<div class='content col-md-6 col-lg-8'>			<h3 class='single-title'>$filter->title</h3>			<div class='portfolio-content'>	<p><i class=\"fa fa-lg fa-history\"></i> $filter->period</p> <br> <p><i class=\"fa fa-lg fa-github\"></i><a href=\"$filter->url\" target=\"_blank\">$filter->url</a> </p>   <br> <p><b>Skills Used: </b>$filter->skills</p> <br>   <p>$filter->description</p>			</div>					</div><div class='close-view'>				<a href='#'><i class='fa fa-times'></i></a>			</div>";
+//    }else{
         $output = "<div class='featured_image col-md-6 col-lg-4'>			                <img  src=$filter->img class='' alt=$filter->title title=''>            		</div>		<div class='content col-md-6 col-lg-8'>			<h3 class='single-title'>$filter->title</h3>			<div class='portfolio-content'>	<p><i class=\"fa fa-lg fa-history\"></i> $filter->period</p> <br><p><b>Skills Used: </b>$filter->skills</p> <br>   <p>$filter->description</p>			</div>					</div><div class='close-view'>				<a href='#'><i class='fa fa-times'></i></a>			</div>";
-    }
+//    }
 
 
     echo $output;
